ce59abffd5663ad7e6c6445e7bf2363ca3f18822
BuildTools <davidmaamoaix@gmail.com>
2022-01-08 11:31:25 +0800

    six face model

--- src/render/model/model.cpp
+++ src/render/model/model.cpp
@@ -1,1 +1,32 @@
 #include "model.h"
+
+// Adds a face with UV coords from the blocks texture map.
+static void addFace(
+    BufferBuilder &buffer, const BlockPos &pos,
+    BlockFace::Facing face, int u, int v
+    ) {
+    for (int i = 0; i < 4; i++) {
+        int vertId = BlockFace::facingVerts[face][i];
+        auto currOffset = BlockFace::vertOffset[vertId];
+        auto uv = BlockFace::getFaceUV(i, u, v);
+        auto norm = BlockFace::facingNormal[face];
+
+        // TODO: add a vertex format
+        buffer.addVert({
+            pos + currOffset, uv,
+            glm::vec3{norm[0], norm[1], norm[2]}
+        });
+    }
+
+    buffer.drawQuad();
+}
+
+SixFaceModel::SixFaceModel(int inU, int inV): u(inU), v(inV) {}
+
+void SixFaceModel::buffer(BufferBuilder &buf, BlockPos pos, bool exposed[6]) {
+    for (auto &face: BlockFace::allFacing) {
+        if (exposed[face]) {
+            addFace(buf, pos, face, u, v);
+        }
+    }
+}
--- src/render/model/model.h
+++ src/render/model/model.h
@@ -5,5 +5,15 @@
 class IModel {
 
 public:
-    //void buffer(BufferBuilder &buffer, bool exposed[6]);
+    virtual void buffer(BufferBuilder &buf, BlockPos pos, bool exposed[6]) = 0;
+};
+
+class SixFaceModel: public virtual IModel {
+
+public:
+    SixFaceModel(int inU, int inV);
+    void buffer(BufferBuilder &buf, BlockPos pos, bool exposed[6]) override;
+
+private:
+    int u, v;
 };
